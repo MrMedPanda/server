824b8a99e8113ddd99bd07c4fd34a68696fa1d3e
MrMedPanda <oleg.mikros@gmail.com>
2022-09-04 21:33:32 +0300

    Add npm scripts and modify people list

--- npm_server/src/data/people.js
+++ npm_server/src/data/people.js
@@ -1,20 +1,20 @@
 const people = {
     list: [
-        { name: 'Oleg', surname: 'Oziava', money: '1000', id: 0 },
-        { name: 'Maks', surname: 'Mamariga', money: '1500', id: 1 },
-        { name: 'Tim', surname: 'Kutuzov', money: '1060', id: 2 },
-        { name: 'Bogdan', surname: 'Zhopodriga', money: '2000', id: 3 },
-        { name: 'Dima', surname: 'Kekyh', money: '1050', id: 4 },
-        { name: 'Ivan', surname: 'Omega', money: '1120', id: 5 },
-        { name: 'Kolia', surname: 'Lepochan', money: '1590', id: 6 },
-        { name: 'Alex', surname: 'Kulibin', money: '1023', id: 7 },
-        { name: 'Kiril', surname: 'Kuplinov', money: '1670', id: 8 },
-        { name: 'Vlad', surname: 'Lemberg', money: '1055', id: 9 },
-        { name: 'Gondon', surname: 'Yakimov', money: '1340', id: 10 },
-        { name: 'Mexicanec', surname: 'Lotos', money: '1980', id: 11 },
-        { name: 'Ricardo', surname: 'Milos', money: '1120', id: 12 },
-        { name: 'Mihail', surname: 'Kumeli', money: '5400', id: 13 },
-        { name: 'Alexey', surname: 'Pidrochan', money: '6700', id: 14 }
+        { name: 'Oleg', surname: 'Oziava', money: '1000', promotion: false, id: 0 },
+        { name: 'Maks', surname: 'Mamariga', money: '1500', promotion: false, id: 1 },
+        { name: 'Tim', surname: 'Kutuzov', money: '1060', promotion: false, id: 2 },
+        { name: 'Bogdan', surname: 'Zhopodriga', money: '2000', promotion: false, id: 3 },
+        { name: 'Dima', surname: 'Kekyh', money: '1050', promotion: false, id: 4 },
+        { name: 'Ivan', surname: 'Omega', money: '1120', promotion: false, id: 5 },
+        { name: 'Kolia', surname: 'Lepochan', money: '1590', promotion: false, id: 6 },
+        { name: 'Alex', surname: 'Kulibin', money: '1023', promotion: false, id: 7 },
+        { name: 'Kiril', surname: 'Kuplinov', money: '1670', promotion: false, id: 8 },
+        { name: 'Vlad', surname: 'Lemberg', money: '1055', promotion: false, id: 9 },
+        { name: 'Gondon', surname: 'Yakimov', money: '1340', promotion: false, id: 10 },
+        { name: 'Mexicanec', surname: 'Lotos', money: '1980', promotion: false, id: 11 },
+        { name: 'Ricardo', surname: 'Milos', money: '1120', promotion: false, id: 12 },
+        { name: 'Mihail', surname: 'Kumeli', money: '5400', promotion: false, id: 13 },
+        { name: 'Alexey', surname: 'Pidrochan', money: '6700', promotion: false, id: 14 }
     ],
     changeList: function (data) {
         this.list = data;
